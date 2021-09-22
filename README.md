ee71732933a74a39f095f96a25568fd69fd18fb5
Rocky43007 <arnabfromtks@gmail.com>
2021-09-22 10:46:51 +0300

    Small update with specific message function

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 Welcome to Rocky's own Discord Client. As usual, he's trying to do something complicated, which he doesn't need to do. 
 
-Test using `npm start` to try the `index.ts` file that Rocky has written for testing.
+
+Steps:
+
+1. Edit `config.json.text` and fill out the information required there. Save the file as `config.json`.
+
+2. Test using `npm start` to try the `index.ts` file that Rocky has written for testing.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 
+import { log } from "console";
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 import { fetchLatestMessage, fetchMessage, fetchMessageSpecific } from "./utils/message.js";
 const require = createRequire(import.meta.url); // construct the require method
@@ -7,6 +8,10 @@ const config = require("../config.json") // use the require method
 const message: Message[] = await fetchMessage(config.token.beta, 'PizzaTown Beta', '889928301366366210');
 const latestMessage: Message | undefined = await fetchLatestMessage(config.token.beta, 'PizzaTown Beta', '889928301366366210');
 const specificMessage = await fetchMessageSpecific(config.token.beta, 'PizzaTown Beta', '889928301366366210', '890134487093084210')
-//console.log(message);
-console.log(latestMessage?.content)
+
+log(message);
+
+log(latestMessage?.content)
+
+log(specificMessage?.content);
 
