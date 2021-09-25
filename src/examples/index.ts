253405d8a9ee833d90b7758c76e6d6fe1c4df800
Rocky43007 <arnabfromtks@gmail.com>
2021-09-25 09:15:11 +0300

    Addition of sub objects in Message

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -5,15 +5,23 @@ import { TextChannelMessage } from '../interfaces/Message';
 
 const client = new Client();
 const msg = new MessageCreator()
+const newlineSpaceRegex = /\n +/g;
+function dedent(multilineString: string): string {
+    return multilineString.replace(newlineSpaceRegex, '\n').trim();
+}
 
 client.on('ready', () => {
     console.log('Bot has logged in!')
 });
 
-client.on('messageCreate', async (message: TextChannelMessage) => {
-    if (message.guild_id !== '746435442521538730') return;
+client.on('messageCreate', async (message) => {
+    console.log(message)
+    if (message.guild.id !== '746435442521538730') return;
     if (message.content === 'Testing') {
-        await msg.send('This is Rocky\'s Own Discord Bot Library working!', message.channel_id)
+        await msg.send('This is Rocky\'s Own Discord Bot Library working!', message.channel.id)
     };
+    if (message.content === 'Avatar') {
+        await msg.send(message.author.displayAvatar(), message.channel.id)
+    }
 })
 client.connect(config.token.beta);
--- src/handlers/MESSAGE_CREATE.ts
+++ src/handlers/MESSAGE_CREATE.ts
@@ -1,6 +1,34 @@
 import Client from "../client/Client";
+import ClientUser from "../client/ClientUser";
 import { Payload } from "../interfaces/payloads";
 
 export default function(client: Client, payload: Payload) {
-    client.emit('messageCreate', payload.d);
+    const msg = payload.d;
+    client.emit('messageCreate', {
+        content: msg.content,
+        channel: {
+            id: msg.channel_id
+        },
+        guild: {
+            id: msg.guild_id
+        },
+        timestamp: msg.timestamp,
+        member: {
+            roles: msg.member.roles,
+            nickname: msg.member.nick,
+        },
+        id: msg.id,
+        embeds: msg.embeds,
+        components: msg.components,
+        author: {
+            username: msg.author.username,
+            id: msg.author.id,
+            discriminator: msg.author.discriminator,
+            avatar: msg.author.avatar,
+            displayAvatar(size: number = 1024) {
+                return `https://cdn.discordapp.com/avatars/361212545924595712/${this.avatar}.webp?size=${size}`
+            }
+        },
+        attachments: msg.attachements || []
+    });
 }
